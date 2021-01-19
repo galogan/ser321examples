7096ac5ca7cd542c842b24dbcf73c9f573bd86bc
galogan <galogan02@gmail.com>
2021-01-19 13:12:33 -0600

    Updated java gradle to match assignment1 requirements 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/READEME.md" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/build.gradle" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/gradle/wrapper/gradle-wrapper.jar" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/gradle/wrapper/gradle-wrapper.properties" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/gradlew" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/gradlew.bat" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/src/main/java/Fraction.java" 	new file:   "Gradle/C\357\200\272UsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/src/main/java/Multiply.java"

---
+++ Gradle/CUsersgalogOneDriveDocumentsGitHubser321-spring2021-A-galoganAssignment1/src/main/java/Fraction.java
@@ -0,0 +1,33 @@
+/**
+ * Purpose: demonstrate simple Java Fraction class with command line,
+ * jdb debugging, and Ant build file.
+ * <p>
+ * Ser321 Foundations of Distributed Applications
+ * see http://pooh.poly.asu.edu/Ser321
+ *
+ * @author Tim Lindquist Tim.Lindquist@asu.edu
+ * Software Engineering, CIDSE, IAFSE, ASU Poly
+ * @version January 2020
+ */
+public class Fraction {
+    public static void main(String[] args) {
+       if(args.length == 2){
+       int argN = 0;
+       int argD = 0;
+        try {
+          argD = Integer.parseInt(args[0]);
+          argN = Integer.parseInt(args[1]);
+        } catch (Exception e)
+        {
+           System.out.println("The arguments you passed " + args[0] + " and " + args[1] + " are not integers");
+           System.exit(1);
+        }
+       System.out.println(" Using the numbers you provided " + argN + " and " + argD + " ,we were able to create your fraction." +
+               "\n Here is your Fraction " + argN + "/" + argD);
+       }else{
+          System.out.println("You need to add only two arguments to create this fraction. " +
+                  "You have either add not enough or one to many.");
+       }
+    }
+
+}
